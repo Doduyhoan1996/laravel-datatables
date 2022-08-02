f99202f14bd2813ae808f6b8b60a3063351ecbb2
hoandd <hoandd@beetsoft.vn>
2022-08-02 16:10:08 +0700

    fix count(): Parameter

--- src/Engines/CollectionEngine.php
+++ src/Engines/CollectionEngine.php
@@ -174,7 +174,7 @@ class CollectionEngine extends BaseEngine
     public function columnSearch()
     {
         $columns = $this->request->get('columns');
-        for ($i = 0, $c = count($columns); $i < $c; $i++) {
+        for ($i = 0, $c = count((array) $columns); $i < $c; $i++) {
             if ($this->request->isColumnSearchable($i)) {
                 $this->isFilterApplied = true;
 
